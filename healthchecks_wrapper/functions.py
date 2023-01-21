79db71825f205950afea86a06971b0369dac0259
Samarpan Rai <samarpan-rai@live.com>
2023-01-21 21:53:32 +0100

    Refactor URL checking code

---
+++ healthchecks_wrapper/functions.py
@@ -0,0 +1,16 @@
+import re
+
+# Shamelessly stolen from Django's url validator https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
+url_regex = re.compile(
+    r"^(?:http|ftp)s?://"  # http:// or https://
+    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+    r"localhost|"  # localhost...
+    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+    r"(?::\d+)?"  # optional port
+    r"(?:/?|[/?]\S+)$",
+    re.IGNORECASE,
+)
+
+
+def is_invalid_url(url: str):
+    return not url_regex.match(url)
