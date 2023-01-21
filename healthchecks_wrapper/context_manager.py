242b05d55d7c56ede6b281a465dcf01b73faabee
Samarpan Rai <samarpan-rai@live.com>
2023-01-21 21:52:31 +0100

    Add entire stack trace to message

--- healthchecks_wrapper/context_manager.py
+++ healthchecks_wrapper/context_manager.py
@@ -1,20 +1,14 @@
 """Main module."""
-import json
-import re
+import traceback
 import socket
-from io import StringIO
 from urllib.request import urlopen
+from .functions import is_invalid_url
 
-# Shamelessly stolen from Django's url validator https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py#L45
-regex = re.compile(
-    r"^(?:http|ftp)s?://"  # http:// or https://
-    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
-    r"localhost|"  # localhost...
-    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
-    r"(?::\d+)?"  # optional port
-    r"(?:/?|[/?]\S+)$",
-    re.IGNORECASE,
-)
+# Constants required by HealthChecks.io
+
+JOB_START_POST_FIX = "/start"
+JOB_END_POST_FIX = ""
+JOB_FAILURE_PATH = "/fail"
 
 
 class HealthCheck:
@@ -25,33 +19,21 @@ class HealthCheck:
             health_check_url (str): A valid url to send request
             suppress_exceptions (bool, optional): [description]. Defaults to False.
         """
-        if not regex.match(health_check_url):
+        if is_invalid_url(health_check_url):
             raise ValueError("Invalid URL provided : {}".format(health_check_url))
         self.health_check_url = health_check_url
         self.suppress_exceptions = suppress_exceptions
 
     def __enter__(self):
-        return self.send_request("/start")
+        return self.send_request(JOB_START_POST_FIX)
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         if exc_type is None:
-            self.send_request("")
+            self.send_request(JOB_END_POST_FIX)
         else:
-            top_stack = StringIO()
-
-            top_lines = top_stack.getvalue().strip("\n").split("\n")[:-4]
-            top_stack.close()
-
-            full_stack = StringIO()
-            full_stack.write("Traceback (most recent call last):\n")
-            full_stack.write("\n".join(top_lines))
-            full_stack.write("\n")
-
-            full_stack.write("{}: {}".format(exc_type.__name__, str(exc_value)))
-            sinfo = full_stack.getvalue().encode("utf-8")
-            full_stack.close()
-
-            self.send_request("/fail", sinfo)
+            stack_trace = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+            payload = stack_trace.encode("utf-8")
+            self.send_request(JOB_FAILURE_PATH, payload)
         return self.suppress_exceptions
 
     def send_request(self, post_fix, text_message=None):
