4ef0370e6303eb86018306c5c6502991a986d93e
Samarpan Rai <samarpan-rai@live.com>
2023-01-24 21:38:09 +0100

    Update version

--- healthchecks_wrapper/__init__.py
+++ healthchecks_wrapper/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Samarpan Rai"""
 __email__ = "samarpan-rai@live.com"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .context_manager import HealthCheck
--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/samarpan-rai/healthchecks_wrapper",
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
 )
